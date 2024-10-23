7de2e63716d491401c1cf52356671038600ac444
dantalan <cruza86@uabc.edu.mx>
2024-10-23 09:28:44 -0700

    env

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,31 +2,30 @@ import './NavBar.css'
 import Link from "../Link/Link"
 import { navItems } from '../../data/NavList'
 import { Link as LinkReact } from 'react-router-dom'
-import {useEffect} from 'react';
 import DarkMode from '../DarkMode/DarkMode';
 
 
-const NavBar = ({countItems}) => {
+const NavBar = ({ countItems }) => {
 
   return <nav>
-    <LinkReact to= '/' className='logo'>Libro Tienda</LinkReact>
+    <LinkReact to='/' className='logo'>Libro Tienda</LinkReact>
     <ul>
-      <DarkMode/>
+      <DarkMode />
       {navItems.map((item) =>
-      <Link 
-        svg = {item.svg} 
-        text = {item.text} 
-        href = {item.href} 
-        isFilled = {false} 
-        isBordered = {false} 
-        id = {item.id}
-        key = {item.id}
-        count = {item.count}
-        number={countItems }>
-      </Link>)}
+        <Link
+          svg={item.svg}
+          text={item.text}
+          href={item.href}
+          isFilled={false}
+          isBordered={false}
+          id={item.id}
+          key={item.id}
+          count={item.count}
+          number={countItems}>
+        </Link>)}
     </ul>
-    
-    </nav>
+
+  </nav>
 }
 export default NavBar
 
---
+++ src/configuration/fb_config.js
@@ -0,0 +1,10 @@
+import config from "../../config";
+export const firebaseConfig = {
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId
+};
